a4c222c8d0ffc88b2d110cf12b877b87cc42042a
Alexander Tchitchigin <sad.ronin@gmail.com>
2020-06-10 10:41:12 +0300

    Less floating point means more performance.

--- src/Ray/Lighting.hs
+++ src/Ray/Lighting.hs
@@ -9,7 +9,7 @@ import SDL (V3(..), V4(..))
 import Ray.Scene.Types (Light(..))
 import qualified Ray.Scene.Types as Light (intensity)
 import Ray.Math.Intersection (Intersection(..))
-import Ray.Math.Vector (dot, norm)
+import Ray.Math.Vector (dot, minus, norm, plus, scaledBy)
 import Ray.Color (Color)
 import qualified Ray.Color as Color
 
@@ -21,10 +21,11 @@ applyLights ambient lights Intersection{..} = transform iColor
   where
     transform (V4 r g b a) =
       let intensity = ambient + computeAll iPoint iNormal lights
-          r1 = round . (*) intensity $ fromIntegral r
-          g1 = round . (*) intensity $ fromIntegral g
-          b1 = round . (*) intensity $ fromIntegral b
-          a1 = round . (*) intensity $ fromIntegral a
+          rint = round $ recip intensity
+          r1 = r `div` rint
+          g1 = g `div` rint
+          b1 = b `div` rint
+          a1 = a `div` rint
        in V4 r1 g1 b1 a1
 
 computeAll :: V3 CFloat -> V3 CFloat -> [Light] -> CFloat
@@ -36,7 +37,7 @@ compute p n light =
       d = n `dot` l
    in clamp d $ (Light.intensity light * d) / (norm n * norm l)
   where
-    dir (PointLight _ pos) = pos - p
+    dir (PointLight _ pos) = pos `minus` p
     dir (DirectionalLight _ d) = d
 
     clamp d x
